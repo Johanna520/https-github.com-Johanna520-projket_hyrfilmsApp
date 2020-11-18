b90a86dfdd84a59a9cacb4a764c8b327f4af88b3
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2020-11-18 15:19:59 +0100

    Model uppstart

---
+++ DatabaseConnection/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseConnection
+{
+    class Model
+    {
+        /*
+        Customer            JOhanna
+        CustomerId (PK)
+        Email
+        FirstName
+        LastName
+        Birthday
+
+        Movie           Jens
+        MovieId (PK)
+        Title
+        Year
+        ImbdRating
+        ImageURL
+        GenreId (FK)
+        LeadactorId (FK)
+
+        Rental      Johanna
+        RentalId
+        RentalDate
+        MovieId (FK)
+        CustomerId (FK)
+
+        Genre           Jens
+        GenreId
+        GenreName
+
+        Leadactor       JOhanna
+        LeadactorId
+        FirstName 
+        LastName    
+
+         */
+    }
+}
