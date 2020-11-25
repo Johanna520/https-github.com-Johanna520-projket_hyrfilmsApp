6df2d285147596ff287d7e66a387327f9cf68554
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2020-11-25 09:29:00 +0100

    lite justeringar

--- DatabaseConnection/Context.cs
+++ DatabaseConnection/Context.cs
@@ -2,13 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
-/*TODO V 48
- * måndag: lägga till bilder i Movies --, få bort upprepning av data i tabellerna ??? Kolla med björn. 
- * tisdag: föreläsning
- * onsdag:GenreMovie, LeadActorMovie - tabellerna. 
- * torsdag: nörja med wpf
- * fredag: tenta
- */
+
 namespace DatabaseConnection
 {
     class Context : DbContext
